4b75ea9b9dad32167bd1dcd8d5ff7c0a760c9f3e
bodyaSokol <47799134+bodyaSokol@users.noreply.github.com>
2023-05-14 23:47:17 +0300

    changed routes with rules of REST API

--- settings/routes.js
+++ settings/routes.js
@@ -6,14 +6,14 @@ module.exports = (app) =>{
     const productsController = require("./../Controller/productsController");
     const ordersController = require("./../Controller/ordersController");
 
-    app.route('/api/orders/create').post(ordersController.create);//устанавливаем роут
+    app.route('/api/orders').post(ordersController.create);//устанавливаем роут
     app.route('/api/searchProducts').post(productsController.productsSearch);//устанавливаем роут
-    app.route('/api/allProducts').get(productsController.getAllProducts);//устанавливаем роут
-    app.route('/api/category/:id').get(categoriesController.getCategoryProducts);//устанавливаем роут
-    app.route('/api/category').get(categoriesController.getAllCategories);//устанавливаем роут
+    app.route('/api/products').get(productsController.getAllProducts);//устанавливаем роут
+    app.route('/api/categories/:id').get(categoriesController.getCategoryProducts);//устанавливаем роут
+    app.route('/api/categories').get(categoriesController.getAllCategories);//устанавливаем роут
     app.route('/api/users/phone/check').post(usersController.checkPhone);//устанавливаем роут
     app.route('/api/users/phone/set').post(usersController.setPhone);//устанавливаем роут
     app.route('/api/users').get(usersController.getAllUsers);//устанавливаем роут
     app.route('/api/users/:id').get(usersController.getUser);//устанавливаем роут
-    app.route('/api/users/create').post(usersController.create);//устанавливаем роут
+    app.route('/api/users').post(usersController.create);//устанавливаем роут
 }
