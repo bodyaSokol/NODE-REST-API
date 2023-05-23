049256fd8fd1c3774469cd0997d167a5c0ef8d59
bodyaSokol <47799134+bodyaSokol@users.noreply.github.com>
2023-05-23 22:53:36 +0300

    Update ordersController.js

--- Controller/ordersController.js
+++ Controller/ordersController.js
@@ -4,6 +4,9 @@ const response = require('./../response');
 const db = require('./../settings/db');
 
 exports.create = (req,res) =>{
+    const bonuces_percent = 0.03;
+
+
     const telegram_id=req.body.telegram_id;
     const bonuces_used=req.body.bonuces_used;
     const delivery_address=req.body.delivery_address;
@@ -16,11 +19,13 @@ exports.create = (req,res) =>{
             const products = req.body.products;
             let done = [];
             let errors_list = [];
+            let final_sum = 0;
             products.forEach(element => {
                 const product_id=element.id;
                 const product_amount=element.amount;
                 const product_price=element.price;
                 const product_discount=element.discount;
+                final_sum = final_sum + ((product_price-product_discount)*product_amount);
                 const sql_create_order_composition = "INSERT INTO `orders_composition` (`order_id`, `product_id`, `amount`,`price`,`discount`) VALUES ('"+order_id+"', '"+product_id+"', '"+product_amount+"', '"+product_price+"', '"+product_discount+"')";
                 db.query(sql_create_order_composition,(error,rows)=>{
                     if(error){
@@ -30,7 +35,23 @@ exports.create = (req,res) =>{
                     }
                 })
             });
-            response.status(200,{id:order_id}, res);
+            let bonuses_accrued = final_sum * bonuces_percent;
+            db.query("SELECT `bonuces_amount` FROM `users` WHERE `telegram_id`='"+telegram_id+"'",(error,rows,fields)=>{
+                if(error){
+                    console.log(error);
+                }else{
+                    let actual_bonuces_amount = rows[0].bonuces_amount;
+                    actual_bonuces_amount = actual_bonuces_amount - bonuces_used + bonuses_accrued;
+                    db.query("INSERT INTO `users` (`bonuces_amount`) VALUES ('"+actual_bonuces_amount+"') WHERE `telegram_id`='"+telegram_id+"'",(error,rows,fields)=>{
+                        if(error){
+                            console.log(error);
+                        }else{
+                            console.log(rows);
+                        }
+                    })
+                }
+            })
+            response.status(200,{id:order_id,bonuses_accrued:bonuses_accrued}, res);
         }
     })
 }
